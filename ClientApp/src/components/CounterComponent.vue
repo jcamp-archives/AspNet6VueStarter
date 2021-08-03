f3b303e89d86bb72ced6ffc5982f0f450ac55134
John Campion <john@brightshore.com>
2021-08-03 19:25:17 -0400

    First draft of app

---
+++ ClientApp/src/components/CounterComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p class="my-3">
+      Current count:
+      <strong>{{ currentCount }}</strong>
+    </p>
+    <button class="mr-2 btn btn-primary" @click.prevent="increment">Increment</button>
+    <button class="btn btn-secondary" @click.prevent="reset">Reset</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+const currentCount = ref(0)
+
+    const increment = (): void => {
+        currentCount.value++
+}
+
+    const reset = (): void => {
+        currentCount.value = 0
+}
+</script>
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AspNet6VueStarter
 A starter template for Vue integrated with .NET 6's new Spa Proxy
+
+NET Portion based on .NET 6's Angular project template
+Vue template based on [Vitesse-Tailwind-Lite](https://github.com/JohnCampionJr/vitesse-tailwind-lite)
+
